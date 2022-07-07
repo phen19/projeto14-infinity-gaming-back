3183a0b267a750e81a6ae7a8c849bf0d4a332f16
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-07-07 12:38:26 -0300

    Chore: Created the schema for the signup route

---
+++ src/schemas/schemas.js
@@ -0,0 +1,9 @@
+import joi from "joi";
+
+
+export const signUpSchema = joi.object({
+    name: joi.string().min(1).required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+    confirmPassword: joi.ref('password')
+});
