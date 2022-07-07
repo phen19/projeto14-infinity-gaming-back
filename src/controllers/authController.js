bb93171da9d1bdfe21d2adbfc13616afcf67e30c
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-07-07 13:08:58 -0300

    Chore: Checks if a user is registered

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,10 @@
 import { db } from "../databases/mongodb.js";
 
 async function signUpUser(req, res) {
+    const signUpUser = req.body;
+    const userExists = await db.collection("users").findOne({ email: signUpUser.email });
 
+    if (userExists) return res.status(401).send("User already registered");
 }
 
 export { signUpUser };
