aba7c6cac49a4685b0db560ccc7a3886144b88e7
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-07-08 18:40:50 -0300

    Chore: Created productsController , productsRoutes and productsRoute

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./src/routes/authRouter.js";
+import productsRouter from "./src/routes/productsRouter.js";
 dotenv.config();
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(json(), cors());
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`The server is running on port ${process.env.PORT}`);
---
+++ src/routes/productsRouter.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { getProducts } from "../controllers/productsController.js";
+
+const productsRouter = Router();
+
+productsRouter.get("/products", getProducts);
+
+export default productsRouter
